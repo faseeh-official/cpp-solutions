d444d47f6e2c6f457628c724d611f5fb85955408
Faseeh <faseeh1080@outlook.com>
2024-10-26 10:18:59 +0530

    Created a linked list for "add_two_numbers".

--- add_two_numbers/add_two_numbers.cpp
+++ add_two_numbers/add_two_numbers.cpp
@@ -1,4 +1,4 @@
-// You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+// You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a reversed linked list.
 
 // You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
@@ -20,3 +20,34 @@
 // Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
 // Output: [8,9,9,9,0,0,0,1]
 
+#include<iostream>
+
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int value) : data(value), next(nullptr) {}
+};
+
+void insertEnd(Node*& head, int value) {
+    Node* newNode = new Node(value);
+    if (!head) {
+        head = newNode;
+        return;
+    }
+    Node* temp = head;
+    while (temp->next)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+void printList(Node* head) {
+    Node* temp = head;
+    while (temp) {
+        std::cout << temp->data << " ";
+        temp = temp->next;
+    }
+    std::cout << std::endl;
+}
